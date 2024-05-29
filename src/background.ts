b32d2d9020d2cf39f224056ab4d6deffcd6e201b
GodPepe7 <privatalexm@gmail.com>
2024-05-29 21:14:49 +0200

    implement downloading subs of current anime episode

--- src/background.ts
+++ src/background.ts
@@ -5,11 +5,42 @@ type Subs = {
   lastModified: string
 }
 
+type JimakuEntry = {
+  id: number
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  const hianimePattern = /https:\/\/hianime\.to\/watch\/.+\?ep=.+/;
+  if (changeInfo.status === 'complete' && tab.url && hianimePattern.test(tab.url)) {
+    console.log("visited hianime")
+    // Inject content script into the tab
+    chrome.scripting.executeScript({ target: { tabId }, files: ['dist/popup.js'] })
+      .then(() => console.log("injected script file"));
+  }
+});
+
 const JIMAKU = "AAAAAAAAAJ4uAS4uQO_LdGQ5XalZvDYLqb2YuYxn6LBCVW3nx-sFwpBywA"
 const BASE_URL = "https://jimaku.cc/api"
 
-async function fetchSubs(id: number, episode: number) {
+async function fetchSubs(title: string, episode: number) {
   try {
+    const encodedSearchQuery = encodeURIComponent(title)
+    const searchResponse = await fetch(`${BASE_URL}/entries/search?query=${encodedSearchQuery}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `${JIMAKU}`
+      }
+    })
+    if (!searchResponse.ok) {
+      console.error(`HTTP error! status: ${searchResponse.status}`)
+      return null
+    }
+    const jimakuEntry: JimakuEntry[] = await searchResponse.json()
+    if (!jimakuEntry) {
+      console.error(`no entries found for ${title}`)
+      return null
+    }
+    const id = jimakuEntry[0].id
     const response = await fetch(BASE_URL + `/entries/${id}/files?episode=${episode}`, {
       method: 'GET',
       headers: {
@@ -17,7 +48,8 @@ async function fetchSubs(id: number, episode: number) {
       }
     })
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      console.error(`HTTP error! status: ${searchResponse.status}`)
+      return null
     }
     const data: Subs[] = await response.json();
     return data
@@ -33,26 +65,25 @@ async function fetchSubs(id: number, episode: number) {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getSubs') {
-    const title = message.episodeTitle
-      (async () => {
-        const subs = await fetchSubs(715, 1)
-        if (!subs) {
-          sendResponse({ success: false, error: "fetching error" });
-          return
+    (async () => {
+      const subs = await fetchSubs(message.animeTitle, message.episode)
+      if (!subs) {
+        sendResponse({ success: false, error: "fetching error" });
+        return
+      }
+      chrome.downloads.download({
+        url: subs[0].url,
+        filename: subs[0].name,
+        saveAs: false
+      }, (downloadId) => {
+        if (chrome.runtime.lastError) {
+          console.error('Download failed:', chrome.runtime.lastError);
+          sendResponse({ success: false, error: chrome.runtime.lastError.message });
+        } else {
+          console.log('Download started:', downloadId);
+          sendResponse({ success: true, downloadId: downloadId });
         }
-        chrome.downloads.download({
-          url: subs[0].url,
-          filename: subs[0].name,
-          saveAs: false
-        }, (downloadId) => {
-          if (chrome.runtime.lastError) {
-            console.error('Download failed:', chrome.runtime.lastError);
-            sendResponse({ success: false, error: chrome.runtime.lastError.message });
-          } else {
-            console.log('Download started:', downloadId);
-            sendResponse({ success: true, downloadId: downloadId });
-          }
-        })
-      })()
+      })
+    })()
   }
 })
--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,4 @@
-chrome.runtime.sendMessage({ action: 'getSubs', episodeTitle: 'Frieren' }).then(message => {
-  console.log(message)
-})
+const animeTitle = document.querySelector("h2.film-name > a")?.textContent
+const episodeElement = document.querySelector(".ssl-item.ep-item.active") as HTMLElement | null
+const episode = episodeElement?.dataset.number;
+chrome.runtime.sendMessage({ action: 'getSubs', animeTitle, episode })
